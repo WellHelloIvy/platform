50cdddf7faa955284c13b88454ee60a15112622b
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-11-26 09:18:54 -0700

    updeted migrations file

---
+++ app/db/migrations/20211126161450-create-user.js
@@ -0,0 +1,46 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING(25),
+        allowNull: false
+      },
+      lastName: {
+        type: Sequelize.STRING(25),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING(50),
+        allowNull: false
+      },
+      hashedPassword: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cashBalance: {
+        type: Sequelize.DECIMAL(10,2),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now')
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
