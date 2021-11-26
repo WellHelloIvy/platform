b34765406c8c0d5264f228bf0f17615eacb4c283
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-11-26 08:44:40 -0700

    deleted contents of db, node mods, and removed sequelize dependancies

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,5 +1,8 @@
 import express from 'express'
+import expressAsyncHandler from 'express-async-handler';
 import apiRouter from './api'
+import User from '../db/models/user'
+
 
 const router = express.Router();
 
@@ -11,6 +14,11 @@ router.get('/hello/world', function(req, res) {
   res.send('Hello World!');
 });
 
+router.get('/users', expressAsyncHandler(async(req, res) => {
+  const user:Array<User> = await User.findAll()
+  console.log('-------------->',User)
+  return user;
+}))
 
 
 export default router;
