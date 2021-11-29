ab67b88bb45a31affaacc024df6db072689cbd47
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-11-29 10:06:00 -0700

    created and tested post route for assets

--- app/routes/api/assets.ts
+++ app/routes/api/assets.ts
@@ -1,22 +1,19 @@
 import express from 'express'
 import expressAsyncHandler from 'express-async-handler';
 import db from '../../db/models'
-import { validateTransaction } from '../../utils/validation';
 
 const router:any = express.Router();
 
 router.post(
   '/',
-  validateTransaction,
   expressAsyncHandler(async(req, res): Promise<any> => {
-    const { userId, cryptoId, price, quantity, buy } = req.body;
-    const transaction = await db.Transaction.create({
-      userId, cryptoId, price, quantity, buy
+    const { userId, cryptoId, quantity } = req.body;
+    const asset = await db.Asset.create({
+      userId, cryptoId, quantity
     })
     return res.json({
-      transaction
+      asset
      })
   })
 )
-
 export default router;
--- app/routes/api/index.ts
+++ app/routes/api/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import sessionRouter from './session';
-import usersRouter from './users'
+import usersRouter from './users';
+import transactionsRouter from './transactions'
+import assetsRouter from './assets'
 
 const router = express.Router();
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
+router.use('/transactions', transactionsRouter);
+router.use('/assets', assetsRouter)
 
 export default router;
