02f3a2265d3198bd1cefe477afe76a3053664973
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-12-02 10:36:01 -0700

    implemented MUI searchbar

--- platform-app/src/components/CryptosPage/index.tsx
+++ platform-app/src/components/CryptosPage/index.tsx
@@ -1,11 +1,14 @@
+import { Paper } from "@mui/material";
 import ReactVirtualizedTable from "components/CryptocurrencyTable";
+import Search from "components/SearchField";
 
 function TradePage(){
 
   return(
-    <>
+    <Paper>
+      <Search />
       <ReactVirtualizedTable />
-    </>
+    </Paper>
   )
 }
 export default TradePage;
