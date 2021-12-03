de8b3ceb4f94cf460a696f69c758a3aa4c3e2c90
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-12-02 20:58:23 -0700

    altered line graph render

--- platform-app/src/components/PriceChart/index.tsx
+++ platform-app/src/components/PriceChart/index.tsx
@@ -4,12 +4,10 @@ import { State } from '../../../module';
 
 const PriceChart = () => {
   const candleStickData = useSelector((state:State) => state.cryptodetails?.candlesticks)
-  // console.log('------------->',candleStickData)
 
-  // const data = [{name: 'Page A', uv: 4000, pv: 2400, amt: 2400},{uv: 2}];
   return(
     <LineChart width={400} height={400} data={candleStickData}>
-      <Line type="monotone" dataKey="closingPrice" stroke="#8884d8" />
+      <Line type="monotone" dataKey="closingPrice" stroke="#8884d8" dot={false} />
     </LineChart>
   )
 }
