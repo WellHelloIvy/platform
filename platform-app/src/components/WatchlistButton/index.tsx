03e09b54c2c71c61b4359f7f7bdb23a2db6a5ea7
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-12-04 16:44:31 -0700

    creating reusable component

--- platform-app/src/components/WatchlistButton/index.tsx
+++ platform-app/src/components/WatchlistButton/index.tsx
@@ -1,8 +1,24 @@
+import { useSelector } from "react-redux"
+import { State } from "../../../module"
 
 
-const WatchlistButton = ({cryptoId}:any) => {
+const WatchlistButton = ({cryptoId, sessionUser}:any) => {
 
-  
+  const watchlistState: any = useSelector((state: State) => state.watchlists)
+  const arrayOfWatchlists = Object.values(watchlistState)
+  const userWatchlists = arrayOfWatchlists.filter((watchlist: any) => watchlist.uderId === sessionUser?.Id)
+  const defaultWatchlist: any = userWatchlists[0]
+  const currenciesOnWatchlist = defaultWatchlist.WatchlistCryptos
+
+  const isInWatchlist = currenciesOnWatchlist.includes(cryptoId)
+
+  return( isInWatchlist?
+    <>yes</>
+    :
+    <>no</>
+
+  )
+
 }
 
 export default WatchlistButton
