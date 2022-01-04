11db5dffd76a905a7491e232d0aaf2187d321f4b
Ivy Huynh <ivy.a.huynh@gmail.com>
2022-01-04 11:10:33 -0700

    rendered cryptocurrencies on data grid

--- frontend/src/components/CryptocurrencyTable/index.tsx
+++ frontend/src/components/CryptocurrencyTable/index.tsx
@@ -1,37 +1,26 @@
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { useSelector } from 'react-redux';
+import { State } from '../../../module';
 
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 90,
-  }
-];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
 
 export default function DataTable() {
+  const cryptocurrencies =Object.values(useSelector((state:State) => state.cryptocurrencies))
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'name', headerName: 'Name', width: 130 },
+  ];
+
+  const rows = cryptocurrencies.slice();
+  
   return (
-    <div style={{ height: 300, width: '100%' }}>
+    <div style={{ height: 300, width: '50%' }}>
       <DataGrid
         rows={rows}
         columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
+        pageSize={100}
+        rowsPerPageOptions={[100]}
       />
     </div >
   );
