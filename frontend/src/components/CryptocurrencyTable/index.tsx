b70d19e21fbd2d4498b96c05a31915c3b238d852
Ivy Huynh <ivy.a.huynh@gmail.com>
2022-01-10 10:22:46 -0700

    styled cryptocurrency table

--- frontend/src/components/CryptocurrencyTable/index.tsx
+++ frontend/src/components/CryptocurrencyTable/index.tsx
@@ -3,30 +3,32 @@ import WatchlistButton from 'components/WatchlistButton';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { State } from '../../../module';
+import './CryptocurrencyTable.css'
 
 
 
-export default function DataTable({ sessionUser }:any) {
+export default function DataTable({ sessionUser }: any) {
   const navigate = useNavigate()
-  const cryptocurrencies = Object.values(useSelector((state:State) => state.cryptocurrencies))
+  const cryptocurrencies = Object.values(useSelector((state: State) => state.cryptocurrencies))
 
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'name', headerName: 'Name', width: 130 },
-    { field: 'watchlist', headerName: 'Add to / Remove from Watchlist', width: 130, renderCell: (params:GridCellParams) =>  (<WatchlistButton cryptoId={params.row.id} sessionUser={sessionUser}/>)},
+    { field: 'id', headerName: 'Ticker', flex: .5 },
+    { field: 'name', headerName: 'Name', flex: 1 },
+    { field: 'watchlist', headerName: 'Add to / Remove from Watchlist', flex: 1, renderCell: (params: GridCellParams) => (<WatchlistButton cryptoId={params.row.id} sessionUser={sessionUser} />) },
   ];
 
   const rows = cryptocurrencies.slice();
 
   return (
-    <div style={{ height: 300, width: '50%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={100}
-        rowsPerPageOptions={[100]}
-        onRowClick={(e) => navigate(`/cryptocurrencies/${e.id}`)}
-      />
-    </div >
+      <div style={{ display: 'flex', height: '100%', width:'100%' }}>
+        <div style={{ flexGrow: 1 }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            onRowClick={(e) => navigate(`/cryptocurrencies/${e.id}`)}
+            disableSelectionOnClick
+          />
+        </div >
+      </div >
   );
 }
--- frontend/src/components/CryptosPage/index.tsx
+++ frontend/src/components/CryptosPage/index.tsx
@@ -1,12 +1,16 @@
 import DataTable from "components/CryptocurrencyTable";
 // import ColumnVirtualizationGrid from "components/CryptocurrencyTable";
 // import ReactVirtualizedTable from "components/CryptocurrencyTable";
+import './CryptosPage.css'
 
 
 function TradePage(){
 
   return(
+    <div className="trading-container">
       <DataTable />
+    </div>
+
   )
 }
 export default TradePage;
